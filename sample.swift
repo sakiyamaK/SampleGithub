f370b346946f4b3f09053172e85f3f850e5171da
sakiyamaK <sakiyama.k@gmail.com>
2020-03-07 11:44:31 +0900

    a

---
+++ sample.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+let test = "今日はいい天気です"
+
+print(test)
+
+print("---------------------")
+
+func method(_ count: Int) {
+    for v in 1...count {
+        print("\(v)回目の実行です")
+    }
+}
+
+
+method(10)
+
+
+print("---------------------")
+
+class TestClass {
+    var name: String
+    var age: Int
+
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+
+    func say() {
+        print("私の名前は「\(name)」です。\(age)歳です")
+    }
+}
+
+let testClass1 = TestClass(name: "あいうおえ", age: 20)
+testClass1.say()
